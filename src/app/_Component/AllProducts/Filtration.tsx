8f9671991358658a565b540cf5ac6ced2bea594a
Omar-Ahmed39 <omarahmed3915@gmail.com>
2026-01-06 05:25:47 +0200

    feat: complete product filtration logic and UI implementation

--- src/app/_Component/AllProducts/Filtration.tsx
+++ src/app/_Component/AllProducts/Filtration.tsx
@@ -1,8 +1,16 @@
 "use client"
 import { CategoryType } from '_/app/_Interfaces/Product.Typs'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React, {useTransition } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
+import { Button } from "_/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "_/components/ui/dropdown-menu"
 
 export type FilterType = {
 
@@ -19,62 +27,129 @@ export type FilterType = {
 
 export default function Filtration({ catagories }: { catagories: CategoryType[] | null }) {
 
-    const { handleSubmit, register } = useForm()
-    const allCatagories = catagories
-    const searchParams = useSearchParams()
-    const setUrl = new URLSearchParams(searchParams)
-    const router = useRouter()
-    const currentPath = usePathname()
+    const allCatagories = catagories;
+    const searchParams = useSearchParams();
+    const setUrl = new URLSearchParams(searchParams);
+    const router = useRouter();
+    const currentPath = usePathname();
 
-    // useEffect(()=>{},[])
-    // setUrl.forEach((param)=>{
-    //     setUrl.delete(`${param}`)
-    // })
+    const { handleSubmit, register } = useForm({
+        defaultValues: {
+            search: setUrl.get('search'),
+            category: setUrl.get('category[in]'),
+            sort: setUrl.get('sort'),
+            'price[gte]': setUrl.get('price[gte]'),
+            'price[lte]': setUrl.get('price[lte]')
+        }
+    });
 
-    const [isLoading, setIsLoading] = useState(false)
+    const [isPending, startTransition] = useTransition();
+
+
     function myHandleSubit(data: FilterType | FieldValues) {
-        console.log(data);
 
-        setIsLoading(true)
+        
         data.price.gte ? setUrl.set('price[gte]', data.price.gte) : setUrl.delete('price[gte]')
-        data.price.lte ?setUrl.set('price[lte]', data.price.lte) : setUrl.delete('price[lte]')
-        data.sort != 'nothing' ? setUrl.set('sort', data.sort) : setUrl.delete('sort')
-        data.category != 'all' ? setUrl.set('category[in]', data.category) : setUrl.delete('category[in]')
+        data.price.lte ? setUrl.set('price[lte]', data.price.lte) : setUrl.delete('price[lte]')
+        data.sort != 'nothing' && data.sort ? setUrl.set('sort', data.sort) : setUrl.delete('sort')
+        data.category != 'all' && data.category ? setUrl.set('category[in]', data.category) : setUrl.delete('category[in]')
         data.search ? setUrl.set('search', data.search) : setUrl.delete('search')
-        router.push(`${currentPath}?${setUrl.toString()}`)
-
+        startTransition(() => {
+            router.push(`${currentPath}?${setUrl.toString()}`, { scroll: false });
+        });
     }
 
 
+
     return (
-        <form className='grid  grid-cols-10 gap-3 mb-5' onSubmit={handleSubmit(myHandleSubit)}>
-            <div className='md:col-span-10 lg:col-span-3 '>
-                <label htmlFor="search" className='block mb-2 text-lg text-[#1F2B4C]'>Search</label>
-                <input type="text" id="#search" placeholder='Search Product' className='w-full rounded-lg' {...register('search')} />
-            </div>
-            <div className='md:col-span-5 lg:col-span-2 '>
-                <label htmlFor="category" className='block mb-2 text-lg text-[#1F2B4C]'>Category</label>
-                <select id='#category' className='w-full rounded-lg' {...register('category')}>
-                    <option value="all">All</option>
-                    {allCatagories?.map((category) => { return <option key={category._id} value={category._id}>{category.name}</option> })}
-                </select>
-            </div>
-            <div className='md:col-span-5 lg:col-span-2 '>
-                <label htmlFor="sort" className='block mb-2 text-lg text-[#1F2B4C]'>Sort By</label>
-                <select id='#sort' className='w-full rounded-lg' {...register('sort')}>
-                    <option value="nothing">Nothing</option>
-                    <option value="price">Price:Low to High</option>
-                    <option value="-price" >Price:High to Low</option>
-                </select>
-            </div>
-            <div className='md:col-span-10 lg:col-span-3 '>
-                <label htmlFor="range" className='block mb-2 text-lg text-[#1F2B4C]'>Price Range</label>
-                <div className='flex  gap-1 '>
-                    <input type="text" id="#range" placeholder='Min Price' className=' rounded-lg w-full' {...register('price[gte]')} />
-                    <input type="text" placeholder='Max Price' className=' rounded-lg w-full' {...register('price[lte]')} />
-                    <button type='submit' className='w-fit  bg-[#102051] px-3 text-white rounded-lg cursor-pointer border-2 hover:border-[#102051] hover:bg-white hover:text-[#102051]  duration-200'>{isLoading ? <i className="fa fa-spinner fa-spin text-2xl text-white" ></i> : 'Apply'}</button>
+
+        <>
+            {/* Filtration from lg screens to en */}
+
+            <form className='  grid-cols-10 gap-3 mb-5 hidden lg:grid' onSubmit={handleSubmit(myHandleSubit)}>
+                <div className='md:col-span-10 lg:col-span-3 '>
+                    <label htmlFor="search" className='block mb-2 text-lg text-[#1F2B4C]'>Search</label>
+                    <input type="text" id="#search" placeholder='Search Product' className='w-full rounded-lg' {...register('search')} />
                 </div>
-            </div>
-        </form>
+                <div className='md:col-span-5 lg:col-span-2 '>
+                    <label htmlFor="category" className='block mb-2 text-lg text-[#1F2B4C]'>Category</label>
+                    <select id='#category' className='w-full rounded-lg' {...register('category')}>
+                        <option value="all">All</option>
+                        {allCatagories?.map((category) => { return <option key={category._id} value={category._id}>{category.name}</option> })}
+                    </select>
+                </div>
+                <div className='md:col-span-5 lg:col-span-2 '>
+                    <label htmlFor="sort" className='block mb-2 text-lg text-[#1F2B4C]'>Sort By</label>
+                    <select id='#sort' className='w-full rounded-lg' {...register('sort')}>
+                        <option value="nothing">Nothing</option>
+                        <option value="price">Price:Low to High</option>
+                        <option value="-price" >Price:High to Low</option>
+                    </select>
+                </div>
+                <div className='md:col-span-10 lg:col-span-3 '>
+                    <label htmlFor="range" className='block mb-2 text-lg text-[#1F2B4C]'>Price Range</label>
+                    <div className='flex  gap-1 '>
+                        <input type="text" id="#range" placeholder='Min Price' className=' rounded-lg w-full' {...register('price[gte]')} />
+                        <input type="text" placeholder='Max Price' className=' rounded-lg w-full' {...register('price[lte]')} />
+                        <button type='submit' disabled={isPending} className='w-fit bg-[#102051] px-3 text-white rounded-lg cursor-pointer border-2 hover:border-[#102051] hover:bg-white hover:text-[#102051] duration-200 min-w-[80px] flex justify-center items-center'>
+                            {isPending ? <i className="fa fa-spinner fa-spin text-xl"></i> : 'Apply'}
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+
+            {/* Filtration from sm screens to md */}
+
+            <DropdownMenu modal={false}>
+                <DropdownMenuTrigger asChild className='w-full py-5 lg:hidden'>
+                    <Button variant="outline" className='mb-8 bg-[#102051] px-3 text-white rounded-lg cursor-pointer border-2 hover:border-[#102051] hover:bg-white hover:text-[#102051]  duration-200 text-xl'>Filters</Button>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent className=" w-90  p-5 z-10 " align="center" >
+                    <form onSubmit={handleSubmit(myHandleSubit)} className='w-full container'>
+                        <div className='pb-2'>
+                            <input type="text" id="#search" placeholder='Search Product' className='w-full rounded-lg ' {...register('search')} />
+                        </div>
+                        <DropdownMenuSeparator />
+                        <div className=' pb-2'>
+                            <label htmlFor="category" className='block mb-2 text-lg text-[#1F2B4C]'>Category</label>
+                            <select id='#category' className='w-full rounded-lg' {...register('category')}>
+                                <option value="all " >All</option>
+                                {allCatagories?.map((category) => { return <option key={category._id} value={category._id}>{category.name}</option> })}
+                            </select>
+                        </div>
+                        <DropdownMenuSeparator />
+                        <div className=' pb-2'>
+                            <label htmlFor="sort" className='block mb-2 text-lg text-[#1F2B4C]'>Sort By</label>
+                            <select id='#sort' className='w-full rounded-lg' {...register('sort')}>
+                                <option value="nothing " >Nothing</option>
+                                <option value="price">Price:Low to High</option>
+                                <option value="-price" >Price:High to Low</option>
+                            </select>
+                        </div>
+                        <DropdownMenuSeparator />
+                        <div className=' pb-2'>
+                            <label htmlFor="range" className='block mb-2 text-lg text-[#1F2B4C]'>Price Range</label>
+                            <div className='flex  gap-1 '>
+                                <input type="text" id="#range" placeholder='Min Price' className=' rounded-lg w-full' {...register('price[gte]')} />
+                                <input type="text" placeholder='Max Price' className=' rounded-lg w-full' {...register('price[lte]')} />
+                            </div>
+                        </div>
+                        <DropdownMenuSeparator />
+
+                        <DropdownMenuItem className='p-0'>
+                            <button type='submit' className='w-full  bg-[#102051] px-3 py-2  text-white rounded-lg cursor-pointer border-2 hover:border-[#102051] hover:bg-white hover:text-[#102051]  duration-200'>Apply</button>
+                        </DropdownMenuItem>
+
+                    </form>
+                </DropdownMenuContent>
+
+
+            </DropdownMenu>
+
+
+        </>
     )
+
 }
